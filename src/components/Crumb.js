5223be96ee757bc837c58a5105c88dbcf666f7c7
joe <joe.chang1014@gmail.com>
2021-11-08 19:35:24 +0800

    add crumb

---
+++ src/components/Crumb.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+const CrumbComp = styled.div`
+  padding-bottom: 20px;
+  font-size: 14px;
+  .crumb-text {
+    color: var(--green);
+  }
+  strong {
+    color: var(--green);
+    margin: 0 5px;
+  }
+`;
+
+export default function Crumb({ type, title }) {
+  const typeList = {
+    activity: "節慶活動",
+    spot: "探索景點",
+    restauran: "品嚐美食",
+  };
+  return (
+    <CrumbComp>
+      <span className="crumb-text">{typeList[type]}</span>
+      <strong>/</strong>
+      <span className="crumb-text">{title}</span>
+    </CrumbComp>
+  );
+}
