d686dfe7c74855d61e50412b4c4f988e3350a2af
joe <joe.chang1014@gmail.com>
2021-11-14 20:08:32 +0800

    update ico

--- src/configs/basic/dev.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-irregular-whitespace */
-export const defaultConfig = {
-  env: "DEV",
-  A008Lot: ["L1_P10", "L3_P10", "L5_P10", "L1_K3", "L3_K3", "L5_K3", "L1_SSC", "L3_SSC", "L5_SSC", "L3_BCO", "L3_LON", "L3_PAR", "L3_SAP", "HCM_VN", "HA_VN"],
-  A008GameSeq: ["SSC", "K3", "P10", "IN", "POK", "MMC", "VN"],
-  pokGameinPlatform: ["VNC"],
-};
